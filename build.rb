0ea1053e4f284334866a3630a6688c4763dea3f7
Johnny Ray <=>
2011-01-14 15:37:00 -0500

    Improvements to build script

--- build.rb
+++ build.rb
@@ -1,61 +1,57 @@
 
-# ======================== strip logging create ==============================
-# creates jquery-swagg-player-nodebug.js
-if File.exists?("js/jquery-swagg-player-nodebug.js")
-	File.delete("js/jquery-swagg-player-nodebug.js");
-end
-file = File.new("js/jquery-swagg-player.js", "r")  
-noComment = File.new("js/jquery-swagg-player-nodebug.js", "w")
-while (line = file.gets)
-	if (not line =~ /LOGGER\./)
-		noComment.puts(line)
-	end
-end
-file.close
-noComment.close
-
-# ======================== strip browser detect ========================
-# creates jquery-swagg-player-no-browser-detect.js
-if File.exists?("js/jquery-swagg-player-nobrowserdetect.js")
-	File.delete("js/jquery-swagg-player-nobrowserdetect.js");
-end
-flag = false;
-file = File.new("js/jquery-swagg-player.js", "r")  
-noBrowserDetect = File.new("js/jquery-swagg-player-nobrowserdetect.js", "w")
-while (line = file.gets)
-	if (flag == false)
-		if (line =~ /BEGIN BROWSER DETECT/)
-			flag = true
-		end
-		if (flag == false) 
-			noBrowserDetect.puts(line)
-		end
-	end
-	if (line =~ /END BROWSER DETECT/)
-		flag = false
-	end
-end
-file.close
-noBrowserDetect.close
-
-# ============================= strip logging from no browser version ===========
-# creates jquery-swagg-player-nodebug-no-browserdetect.js
-if File.exists?("js/jquery-swagg-player-nodebug-nobrowserdetect.js")
-	File.delete("js/jquery-swagg-player-nodebug-nobrowserdetect.js");
-end
-file = File.new("js/jquery-swagg-player-nobrowserdetect.js", "r")  
-noDebugNoBrowser = File.new("js/jquery-swagg-player-nodebug-nobrowserdetect.js", "w")
-while (line = file.gets)
-	if (not line =~ /LOGGER\./)
-		noDebugNoBrowser.puts(line)
-	end
+# strips the lines of a file which have LOGGER. in them as these are
+# presumably debugging output
+# output: creates jquery-swagg-player-nodebug.js
+def stripLog(baseFile, newFile)
+  puts "Stripping log statements from " + baseFile
+  if File.exists?(newFile)
+    File.delete(newFile);
+  end
+  file = File.new(baseFile, "r")  
+  noComment = File.new(newFile, "w")
+  while (line = file.gets)
+    if (not line =~ /LOGGER\./)
+      noComment.puts(line)
+    end
+  end
+  file.close
+  noComment.close
 end
 
+# finds the beginning of the BrowserDetect as denoted with comments
+# and strips them
+# output: creates jquery-swagg-player-no-browser-detect.js
+def stripBrowserDetect(baseFile, newFile)
+  puts "Stripping BrowserDetect from " + baseFile
+  if File.exists?(newFile)
+    File.delete(newFile);
+  end
+  flag = false;
+  file = File.new(baseFile, "r")  
+  noBrowserDetect = File.new(newFile, "w")
+  while (line = file.gets)
+    if (flag == false)
+      if (line =~ /BEGIN BROWSER DETECT/)
+        flag = true
+      end
+      if (flag == false) 
+        noBrowserDetect.puts(line)
+      end
+    end
+    if (line =~ /END BROWSER DETECT/)
+      flag = false
+    end
+  end
+  file.close
+  noBrowserDetect.close
+end
+
+stripLog("js/jquery-swagg-player.js", "js/jquery-swagg-player-nodebug.js")
+stripBrowserDetect("js/jquery-swagg-player.js", "js/jquery-swagg-player-nobrowserdetect.js")
+stripLog("js/jquery-swagg-player-nobrowserdetect.js", "js/jquery-swagg-player-nodebug-nobrowserdetect.js")
 
 # ============================== minifiy =========================================
 
-file.close
-noDebugNoBrowser.close
 
 puts "Compressing Swagg Player"
 system "juicer merge -i --force js/jquery-swagg-player.js"
@@ -67,3 +63,5 @@ puts "Compressing Swagg Player (No Logging and no BrowserDetect)"
 system "juicer merge -i --force js/jquery-swagg-player-nodebug-nobrowserdetect.js"
 
 puts "Done!"
+
+
